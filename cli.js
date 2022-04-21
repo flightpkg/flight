bfe7e10a9794bc5c04f4bc60eda36b3f2fe32d30
DevComp <hi@devcomp.tk>
2022-04-21 16:43:05 +0530

    :sparkles: Add publish command using wrapper

--- cli.js
+++ cli.js
@@ -1,14 +1,30 @@
+#!/usr/bin/env node
+
 const args = process.argv.slice(2);
 const { help_menu } = require('./src/constants');
 const lib = require('./src/js/lib')
+const { exec } = require("child_process");
 
 if (args[0] == "-js" || args[0] == "--js") {
   if (args[1] == "install" || args[1] == "i") {
     lib.get()
   } else if (args[1] == "uninstall") {
     lib.uninstall(args[1])
+  } else if (args[1] == "publish") {
+    exec("./bin/wrapper-js.js npm publish", (error, stdout, stderr) => {
+      if (error) {
+          console.log(`ERROR: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.log(`${stderr}`);
+          return;
+      }
+      console.log(`${stdout}`);
+    });
   } 
 } else if (args[0] == undefined  || args[0] == "--help" || args[0] == "-h") {
 
   process.stdout.write(help_menu)
 }
+
---
+++ src/js/publisher/publish.sh
@@ -0,0 +1,1 @@
+./bin/wrapper-js.js npm publish
