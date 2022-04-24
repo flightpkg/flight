4eaa525fb2cf6b78c4f1bcadd0ee4b479986b3cd
DevComp <hi@devcomp.tk>
2022-04-24 12:31:29 +0530

    :sparkles: Add rust installation & compile system

--- cli.js
+++ cli.js
@@ -2,15 +2,16 @@
 
 const args = process.argv.slice(2);
 const { help_menu } = require('./src/constants');
-const lib = require('./src/js/lib')
+const lib_js = require('./src/js/lib')
+const lib_rs = require('./src/rs/lib')
 const { exec } = require("child_process");
 const cp = require("child_process");
 
 if (args[0] == "-js" || args[0] == "--js") {
   if (args[1] == "install" || args[1] == "i") {
-    lib.get()
+    lib_js.get()
   } else if (args[1] == "uninstall") {
-    lib.uninstall(args[1])
+    lib_js.uninstall(args[1])
   } else if (args[1] == "publish") {
     const child = cp.exec('./src/js/publisher/publish.sh', {stdio: "inherit"})
     child.stdout.on('data', (data) => {
@@ -20,9 +21,14 @@ if (args[0] == "-js" || args[0] == "--js") {
   child.stderr.on('data', (data) => {
     console.error(`ERROR:\n${data}`);
   });
-  } 
 } else if (args[0] == undefined  || args[0] == "--help" || args[0] == "-h") {
 
   process.stdout.write(help_menu)
 }
+  } else if (args[0] == "-rs" || args[0] == "--rs") {
+    if (args[1] == undefined) {
+      process.stdout.write(help_menu)
+    } else if (args[1] !== undefined) {
+      lib_rs.run(args[1])
 
+}}
