4713bb58bfdb1df7ae0eb1477693ccd530b42969
Flight CI <hi@devcomp.tk>
2022-05-12 09:24:29 +0000

    📦️ Compiled CLI & Bins

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be07d16eb9173bc10605d80924edfaeb53a011c6f40d16deb7bcb84c953f657b
-size 1699458
+oid sha256:8f95f270e84e880db57e3b625b5180139d2b2efa09c3db89f0c266c1931a2d0c
+size 1766547
